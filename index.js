dbfbaa12d9de800a880a291d7ac88fa7c7c40d96
Alberto Iturbe <alberto@vectortranslations.com>
2024-08-03 23:20:23 +0200

    20240803

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const myHeading = document.getElementById("myHeading");
+myHeading.style.textAlign = "center";
+
 const solution = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 for (let i = solution.length - 1; i > 0; i--) {
   let j = Math.floor(Math.random() * (i + 1));
@@ -21,7 +24,7 @@ s789 = s7 * s8 * s9;
 s147 = s1 * s4 * s7;
 s258 = s2 * s5 * s8;
 s369 = s3 * s6 * s9;
-enter = "NA";
+enterButton = "ENTRAR";
 
 document.getElementById("s123").innerHTML = s123;
 document.getElementById("s456").innerHTML = s456;
@@ -29,7 +32,7 @@ document.getElementById("s789").innerHTML = s789;
 document.getElementById("s147").innerHTML = s147;
 document.getElementById("s258").innerHTML = s258;
 document.getElementById("s369").innerHTML = s369;
-document.getElementById("enter").innerHTML = enter;
+document.getElementById("enterButton").innerHTML = enterButton;
 
 function checkCorrectness(u1, u2, u3, u4, u5, u6, u7, u8, u9) {
   userInputCorrect =
@@ -39,7 +42,7 @@ function checkCorrectness(u1, u2, u3, u4, u5, u6, u7, u8, u9) {
     u1 * u4 * u7 == s147 &&
     u2 * u5 * u8 == s258 &&
     u3 * u6 * u9 == s369;
-  if (userInputCorrect == true) {
+  if (userInputCorrect) {
     window.alert("Correcto");
   } else {
     window.alert("Incorrecto");
